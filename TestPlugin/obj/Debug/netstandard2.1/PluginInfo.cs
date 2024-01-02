e2c6b9dc26b5a2e044365b5e0985e3c2f426cf6d
PatikHeffley <127326236+PatikHeffley@users.noreply.github.com>
2024-01-01 22:21:07 -0700

    first commit

---
+++ TestPlugin/obj/Debug/netstandard2.1/PluginInfo.cs
@@ -0,0 +1,9 @@
+namespace TestPlugin
+{
+    public static class PluginInfo
+    {
+        public const string PLUGIN_GUID = "TestPlugin";
+        public const string PLUGIN_NAME = "TestPlugin";
+        public const string PLUGIN_VERSION = "1.0.0";
+    }
+}
