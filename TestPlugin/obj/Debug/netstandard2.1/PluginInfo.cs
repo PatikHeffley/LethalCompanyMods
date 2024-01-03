39cb2a3715bb5b462e588c87628873559fca8511
PatikHeffley <127326236+PatikHeffley@users.noreply.github.com>
2024-01-02 19:06:40 -0700

    Delete TestPlugin directory

--- TestPlugin/obj/Debug/netstandard2.1/PluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace TestPlugin
-{
-    public static class PluginInfo
-    {
-        public const string PLUGIN_GUID = "TestPlugin";
-        public const string PLUGIN_NAME = "TestPlugin";
-        public const string PLUGIN_VERSION = "1.0.0";
-    }
-}
